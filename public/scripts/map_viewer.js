9350df0b4385710b8595f2aff15d80e424a70b6b
Peter Goshulak <pgoshulak@gmail.com>
2018-05-03 21:58:50 -0400

    Add partials and basic map viewer

--- public/scripts/map_viewer.js
+++ public/scripts/map_viewer.js
@@ -1,1 +1,7 @@
-console.log('hello from map_viewer.js')
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: new google.maps.LatLng(43.7, -79.4)
+  })
+}
